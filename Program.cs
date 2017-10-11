2e177de6bde30e4802b29d12b8cf717466de15de
FIC <lucas_redigolo@hotmail.com>
2017-10-10 21:34:21 -0300

    inclui comentários

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace classesStream
         {
             Console.WriteLine("Hello World!");
 
-            StreamWriter sw = new StreamWriter("Arquivo.txt"); //Criar um arquivo novo na pasta do projeto
-            
-            sw.Write("Teste"); // Usado para acrescentar algo no arquivo especificado na variavel acima
+            StreamWriter sw = new StreamWriter("Arquivo.txt"); //Criar um arquivo novo na pasta do projeto ---- se eu incluir ", true" vai atualizando ao invés de sobreescrever
+
+            sw.WriteLine("Teste"); // Usado para acrescentar algo no arquivo especificado na variavel acima
+            sw.Write("teste2"); // escrever em uma linha separada 
             sw.Close();
 
+            FileInfo fi= new FileInfo("Arquivo.txt"); // uma variavel para incluir os dados do arquivo
+            System.Console.WriteLine("Tamanho " + fi.Length + " caracteres");
+            System.Console.WriteLine("Criado em " + fi.CreationTime);
+            System.Console.WriteLine("Extensão "+ fi.Extension);
+                
         }
     }
 }
