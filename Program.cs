7bb4c8b4ef227ec31ee1b4cf261d67350a00ddd9
FIC <lucas_redigolo@hotmail.com>
2017-10-10 21:08:08 -0300

    novos estudos de StreamWritter

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace classesStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            StreamWriter SW = new StreamWriter("Arquivo.txt"); //Criar um arquivo novo na pasta do projeto
+        }
+    }
+}
